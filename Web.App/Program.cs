f7b4a160fa17b888d2286e8301e6052f78182e3d
beauchama <beauchama@kappaduck.com>
2026-04-03 13:22:42 -0400

    Kappaduck-2026-04-03

---
+++ Web.App/Program.cs
@@ -0,0 +1,13 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Web.App;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+WebAssemblyHost host = builder.Build();
+await host.RunAsync();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# KappaDuck
+
+A portfolio that showcases the projects I’ve built and am currently working on.
