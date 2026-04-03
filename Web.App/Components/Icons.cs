945b351cdeadfccefa6d7c1c2d757d63146ebbe8
beauchama <beauchama@kappaduck.com>
2026-04-03 13:38:15 -0400

    Added NavMenu

---
+++ Web.App/Components/Icons.cs
@@ -0,0 +1,23 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Web.App.Components;
+
+internal static class Icons
+{
+    internal const string BoxArrowUpRight = "bi bi-box-arrow-up-right";
+
+    internal const string Controller = "bi bi-controller";
+
+    internal const string Github = "bi bi-github";
+
+    internal const string Joystick = "bi bi-joystick";
+
+    internal const string Moon = "bi bi-moon";
+
+    internal const string Sun = "bi bi-sun";
+
+    internal const string Terminal = "bi bi-terminal";
+
+    internal const string Window = "bi bi-window";
+}
